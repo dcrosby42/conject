8f705117b66074420ed42aa6c5f4af52a8550379
David Crosby <dcrosby42@gmail.com>
2012-01-07 17:27:59 -0500

    Initial parent context lookup

--- lib/object_context.rb
+++ lib/object_context.rb
@@ -1,5 +1,12 @@
+class ObjectContext; end
+
+# The rest of the libraries namespace themselves under ObjectContext so
+# they must be required AFTER the initial definition of ObjectContext class.
+require 'object_context/object_definition'
+require 'object_context/class_ext_construct_with'
+
 class ObjectContext
-  # construct_with :parent_context, :object_factory
+  construct_with :parent_context#, :object_factory
 
   def initialize
     @cache = {}
@@ -12,12 +19,11 @@ class ObjectContext
 
   def get(name)
     object = @cache[name]
-    return object
-    # object = @cache[name]
-    # return object if !object.nil?
-    # 
-    # if parent_context and parent_context.contains(name)
-    #   return parent_context.get(name)
+    return @cache[name] if @cache.keys.include?(name)
+    
+    if parent_context and parent_context.has?(name)
+      return parent_context.get(name)
+    end
     # else
     #   object = object_factory.construct_new(name, :in_context => self)
     #   @cache[name] = object
@@ -26,8 +32,4 @@ class ObjectContext
   end
 end
 
-# The rest of the libraries namespace themselves under ObjectContext so
-# they must be required AFTER the initial definition of ObjectContext class.
-require 'object_context/object_definition'
-require 'object_context/class_ext_construct_with'
 
--- spec/object_context_spec.rb
+++ spec/object_context_spec.rb
@@ -1,6 +1,12 @@
 require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
 
 describe ObjectContext do
+  subject do
+    ObjectContext.new(:parent_context => parent_context)
+  end
+
+  let :parent_context do mock(:parent_context) end
+
   describe "#get" do
     describe "when an object has been #put" do
       before { subject.put(:kraft, "verk") }
@@ -12,10 +18,22 @@ describe ObjectContext do
 
     describe "when the object is not in the cache" do
       describe "and the parent context has got the requested object" do
-        it "returns the object from the parent context"
+        before do
+          parent_context.should_receive(:has?).with(:cheezburger).and_return(true)
+          parent_context.should_receive(:get).with(:cheezburger).and_return("can haz")
+        end
+
+        it "returns the object from the parent context" do
+          subject.get(:cheezburger).should == "can haz"
+        end
       end
 
       describe "and the parent context does NOT have the requested object" do
+        it "constructs the object anew using the object factory" 
+        it "caches the results of using the object facatory"
+      end
+
+      describe "but there is no parent context" do
         it "constructs the object anew using the object factory"
         it "caches the results of using the object facatory"
       end

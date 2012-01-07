c861109ef13ed6050475f2304f11be56c280a972
David Crosby <dcrosby42@gmail.com>
2012-01-07 10:03:58 -0500

    Moved specs into object_context subdirectorty

--- spec/object_context/extended_metaid_spec.rb
+++ spec/object_context/extended_metaid_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
+require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 
 #
 # This set of specs isn't intended to exhaust metaid's functionality.

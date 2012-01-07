960099fc2c3bf49d7bebe1f7aece8bc20b632e91
David Crosby <dcrosby42@gmail.com>
2012-01-07 17:51:35 -0500

    ObjectContext get, put and has? implemented in terms of parent context and object factory

--- lib/object_context.rb
+++ lib/object_context.rb
@@ -6,11 +6,11 @@ require 'object_context/object_definition'
 require 'object_context/class_ext_construct_with'
 
 class ObjectContext
-  construct_with :parent_context#, :object_factory
+
+  construct_with :parent_context, :object_factory
 
   def initialize
     @cache = {}
-    # @cache[:this_context] = self
   end
 
   def put(name, object)
@@ -23,12 +23,24 @@ class ObjectContext
     
     if parent_context and parent_context.has?(name)
       return parent_context.get(name)
+    else
+      object = object_factory.construct_new(name,self)
+      @cache[name] = object
+      return object
     end
-    # else
-    #   object = object_factory.construct_new(name, :in_context => self)
-    #   @cache[name] = object
-    #   return object
-    # end
+  end
+
+  def has?(name)
+    i_have_it = @cache.keys.include?(name)
+    return true if i_have_it
+
+    # Ask parent (if i have a parent) if I don't have the object:
+    if !parent_context.nil?
+      return parent_context.has?(name)
+    else
+      # I don't have it.  My parent doesn't have it.
+      return false
+    end
   end
 end
 
--- spec/object_context_spec.rb
+++ spec/object_context_spec.rb
@@ -2,10 +2,11 @@ require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
 
 describe ObjectContext do
   subject do
-    ObjectContext.new(:parent_context => parent_context)
+    ObjectContext.new(:parent_context => parent_context, :object_factory => object_factory)
   end
 
   let :parent_context do mock(:parent_context) end
+  let :object_factory do mock(:object_factory) end
 
   describe "#get" do
     describe "when an object has been #put" do
@@ -29,25 +30,80 @@ describe ObjectContext do
       end
 
       describe "and the parent context does NOT have the requested object" do
-        it "constructs the object anew using the object factory" 
-        it "caches the results of using the object facatory"
+        before do
+          parent_context.should_receive(:has?).with(:cheezburger).and_return(false)
+          parent_context.should_not_receive(:get)
+          @but_i_ated_it = Object.new
+          object_factory.should_receive(:construct_new).with(:cheezburger, subject).and_return(@but_i_ated_it)
+        end
+
+        it "constructs the object anew using the object factory" do
+          subject.get(:cheezburger).should == @but_i_ated_it
+        end
+
+        it "caches the results of using the object facatory" do
+          subject.get(:cheezburger).should == @but_i_ated_it
+          # mock object factory would explode if we asked it twice:
+          subject.get(:cheezburger).should == @but_i_ated_it
+        end
+
       end
 
       describe "but there is no parent context" do
-        it "constructs the object anew using the object factory"
-        it "caches the results of using the object facatory"
+        let :parent_context do nil end
+        before do
+          @but_i_ated_it = Object.new
+          object_factory.should_receive(:construct_new).with(:cheezburger, subject).and_return(@but_i_ated_it)
+        end
+
+        it "constructs the object anew using the object factory" do
+          subject.get(:cheezburger).should == @but_i_ated_it
+        end
+
+        it "caches the results of using the object facatory" do
+          subject.get(:cheezburger).should == @but_i_ated_it
+          # mock object factory would explode if we asked it twice:
+          subject.get(:cheezburger).should == @but_i_ated_it
+        end
       end
     end
   end
 
   describe "#has?" do
     describe "when the object exists in the cache" do
-      it "returns true"
+      describe "due to #put" do
+        it "returns true" do
+          subject.put(:a_clue, "Wodsworth")
+          subject.has?(:a_clue).should be_true
+          subject.has?(:a_clue).should be_true
+        end
+      end
+      describe "due to caching a previous #get" do
+        before do
+          parent_context.stub(:has?).and_return(false)
+          object_factory.stub(:construct_new).and_return("Mr Green")
+        end
+        it "returns true" do
+          subject.get(:a_clue).should == "Mr Green"
+          subject.has?(:a_clue).should be_true
+          subject.has?(:a_clue).should be_true
+        end
+      end
     end
+
     describe "when the object does NOT exist in the cache" do
-      it "returns false"
-      describe "when the parent context is set" do
-        it "returns true"
+      describe "when there is no parent context" do
+        let :parent_context do nil end
+        it "returns false" do
+          subject.has?(:a_clue).should == false
+        end
+      end
+
+      describe "when there is a parent context" do
+        it "delegates the question to the parent context" do
+          parent_context.should_receive(:has?).with(:a_clue).and_return("the parent answer")
+          subject.has?(:a_clue).should == "the parent answer"
+        end
       end
     end
   end

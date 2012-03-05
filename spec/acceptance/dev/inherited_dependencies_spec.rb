f7c4179500cdc0e958f4fa9a5fa6cc3a64db0813
Shawn Anderson <shawn42@gmail.com>
2012-03-05 13:14:21 -0500

    added test for inheritance; now properly ignore Gemfile.lock

---
+++ spec/acceptance/dev/inherited_dependencies_spec.rb
@@ -0,0 +1,26 @@
+require File.expand_path(File.dirname(__FILE__) + "/../../spec_helper")
+
+describe "basic inheritance " do
+  subject { Conject.default_object_context }
+
+  before do
+    append_test_load_path "basic_composition"
+    require 'lobby'
+    require 'front_desk'
+    require 'guest'
+    require 'special_guest'
+    require 'tv'
+  end
+
+  it 'is built with parental dependencies' do
+    tv = subject[:tv]
+    front_desk = subject[:front_desk]
+    original_guest = subject[:guest]
+
+    guest = subject[:special_guest]
+    guest.should be
+    guest.tv.should == tv
+    guest.front_desk.should == front_desk
+    guest.foo.should == 'foo'
+  end
+end

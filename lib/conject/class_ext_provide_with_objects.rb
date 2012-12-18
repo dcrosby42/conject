14afbf8cb85f1df0f6907149f0fb3bc8032c43de
David Crosby <dcrosby42@gmail.com>
2012-12-18 09:28:30 -0500

    Preliminary implementation of provide_with_objects

---
+++ lib/conject/class_ext_provide_with_objects.rb
@@ -0,0 +1,19 @@
+require 'conject/utilities'
+class Class
+  def provide_with_objects(*syms)
+    klass = self
+
+    # Give instances of this class the ability to ref their object context
+    klass.class_def_private :object_context do
+      @_conject_object_context || Thread.current[:current_object_context] #|| Conject.default_object_context
+    end
+
+    # Define an internal reader method per dependency:
+    syms.each do |object_name|
+      mname = Conject::Utilities.generate_accessor_method_name(object_name)
+      class_def_private mname do
+        object_context[object_name]
+      end
+    end
+  end
+end
---
+++ spec/acceptance/dev/provide_with_objects_spec.rb
@@ -0,0 +1,29 @@
+require File.expand_path(File.dirname(__FILE__) + "/../../spec_helper")
+
+describe "lazy dependency resolution via provide_with_objects" do
+  subject { new_object_context }
+
+  before do
+    append_test_load_path "lazy_resolution"
+    require 'hobbit/baggins'
+    require 'hobbit/shire'
+    require 'hobbit/precious'
+  end
+
+  after do
+    restore_load_path
+  end
+
+  describe "for 'regular' context objects (instances)" do
+    it "provides objects" do
+      baggins = subject["hobbit/baggins"]
+      baggins.to_s.should == "From the Shire, found the One Ring"
+    end
+  end
+
+  describe "mixed lazy and construct with"
+  describe "provide SELF with objects"
+  describe "objects injected into context after construction"
+
+end
+
